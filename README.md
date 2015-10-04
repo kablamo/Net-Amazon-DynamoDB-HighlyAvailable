4d6bbdddda63a1ec128723b7426247e4f9299373
Eric Johnson <eric.git@iijo.org>
2015-10-03 21:43:49 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Net::Amazon::DynamoDB::HighlyAvailable - It's new $module
+
+# SYNOPSIS
+
+    use Net::Amazon::DynamoDB::HighlyAvailable;
+
+# DESCRIPTION
+
+Net::Amazon::DynamoDB::HighlyAvailable is ...
+
+# LICENSE
+
+Copyright (C) Eric Johnson.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Eric Johnson <eric.git@iijo.org>
---
+++ lib/Net/Amazon/DynamoDB/HighlyAvailable.pm
@@ -0,0 +1,39 @@
+package Net::Amazon::DynamoDB::HighlyAvailable;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Net::Amazon::DynamoDB::HighlyAvailable - It's new $module
+
+=head1 SYNOPSIS
+
+    use Net::Amazon::DynamoDB::HighlyAvailable;
+
+=head1 DESCRIPTION
+
+Net::Amazon::DynamoDB::HighlyAvailable is ...
+
+=head1 LICENSE
+
+Copyright (C) Eric Johnson.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Eric Johnson E<lt>eric.git@iijo.orgE<gt>
+
+=cut
+
